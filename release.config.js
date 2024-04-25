18da649608716631dc55a4c0fc3510ee17798d74
Alexander Thoma <thomalexander92@outlook.com>
2024-04-25 21:24:44 +0200

    ci: install semantic-release plugins in pipeline

--- release.config.js
+++ release.config.js
@@ -2,11 +2,21 @@ const config = {
     branches: ['main'],
     plugins: [
         '@semantic-release/commit-analyzer',
+        
         '@semantic-release/release-notes-generator',
+
+        [
+            '@semantic-release/changelog',
+            {
+                changelogFile: 'CHANGELOG.md'
+            }
+        ],
+        
         ["@semantic-release/git", {
-            "assets": ["dist/*.js", "dist/*.js.map"],
+            "assets": ["dist/**", "CHANGELOG.md"],
             "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
         }],
+        
         '@semantic-release/github'
     ]
 };
